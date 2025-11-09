d2cfb2f859ef11855017edd7a3c239c5977c74ad
Seifeda <seifebekele07@gmail.com>
2025-11-09 14:15:32 +0300

    update crud.d.ts insertRoe

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -3,8 +3,10 @@
 import { RowID, RowElement } from './interface';
 
 // Ambient declarations of the CRUD functions
-export declare function insertRow(row: RowElement): RowID;
+
+declare function insertRow(row: RowElement): number
 export declare function updateRow(rowId: RowID, row: RowElement): RowID;
 export declare function deleteRow(rowId: RowID): void;
 export declare function getRow(rowId: RowID): RowElement | null;
+
 
