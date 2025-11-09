5132d05499273eab825cdcca5e2af437c0196db7
Seifeda <seifebekele07@gmail.com>
2025-11-09 13:38:31 +0300

    update task 2 teachClass function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -85,6 +85,7 @@ export function teachClass(todayClass: Subjects): string {
 }
 
 
+
 // Example usage
 console.log(teachClass('Math'));     // Teaching Math
 console.log(teachClass('History'));  // Teaching History
