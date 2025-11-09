dcb103b94e68821da5847dc45449c1d381837b5d
Seifeda <seifebekele07@gmail.com>
2025-11-09 13:20:07 +0300

    update task 2 createEmployee function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -38,9 +38,10 @@ class Teacher implements TeacherInterface {
   }
 }
 
+
 // Function to create employee based on salary
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
+  if (salary < 500) {          // literal check
     return new Teacher();
   }
   return new Director();
