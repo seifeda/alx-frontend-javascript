4ecabf65a08c40342ac2b48710358a0f5a5a643c
Seifeda <seifebekele07@gmail.com>
2025-11-09 14:03:18 +0300

    adding crud.js

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,21 +1,7 @@
-// task_3/js/crud.d.ts
-
 /// <reference path="./interface.ts" />
 
 import { RowID, RowElement } from './interface';
 
-export function insertRow(row) {
-  console.log('Insert row', row);
-  return Math.floor(Math.random() * Math.floor(1000));
-}
-
-export function deleteRow(rowId) {
-  console.log('Delete row id', rowId);
-  return;
-}
-
-export function updateRow(rowId, row) {
-  console.log(`Update row ${rowId}`, row);
-
-  return rowId;
-}
+declare function insertRow(row: RowElement): RowID;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
+declare function deleteRow(rowId: RowID): void;
